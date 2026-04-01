d05f5fe42d054e7ec5e2ad05c7187b546a09a34a
3-x-9 <tino.brazda@gmail.com>
2026-04-01 22:24:50 +0200

    switched lib, fixed error to stderr

--- gvs_core.c
+++ gvs_core.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <pigpio.h>
+#include <pigpiod_if2.h>
 
 #define PIN_B1 18
 #define PIN_B2 19
@@ -12,15 +12,17 @@ typedef struct {
 } GVSState;
 
 GVSState state;
+int pi_handle = -1;
 
 int gvs_init() {
-    if (gpioInitialise() < 0) {
-        printf("Unable to connect to pigpiod\n");
+    pi_handle = pigpio_start(NULL, NULL);
+    if (pi_handle < 0) {
+        fprintf(stderr, "Unable to connect to pigpiod\n");
         return -1;
     }
 
-    gpioSetMode(PIN_B1, PI_OUTPUT);
-    gpioSetMode(PIN_B2, PI_OUTPUT);
+    set_mode(pi_handle, PIN_B1, PI_OUTPUT);
+    set_mode(pi_handle, PIN_B2, PI_OUTPUT);
 
     state.frequency = 100;
     state.duty_cycle = 1000000;
@@ -32,11 +34,11 @@ int gvs_init() {
 
 int gvs_start() {
     if (state.direction == 0) {
-        gpioHardwarePWM(PIN_B1, state.frequency, state.duty_cycle);
-        gpioHardwarePWM(PIN_B2, state.frequency, 0);
+        hardware_PWM(pi_handle, PIN_B1, state.frequency, state.duty_cycle);
+        hardware_PWM(pi_handle, PIN_B2, state.frequency, 0);
     } else {
-        gpioHardwarePWM(PIN_B1, state.frequency, 0);
-        gpioHardwarePWM(PIN_B2, state.frequency, state.duty_cycle);
+        hardware_PWM(pi_handle, PIN_B1, state.frequency, 0);
+        hardware_PWM(pi_handle, PIN_B2, state.frequency, state.duty_cycle);
     }
 
     state.running = 1;
@@ -44,15 +46,15 @@ int gvs_start() {
 }
 
 int gvs_stop() {
-    gpioHardwarePWM(PIN_B1, 0, 0);
-    gpioHardwarePWM(PIN_B2, 0, 0);
+    hardware_PWM(pi_handle, PIN_B1, 0, 0);
+    hardware_PWM(pi_handle, PIN_B2, 0, 0);
     state.running = 0;
     return 0;
 }
 
 int set_intensity(int percent) {
     if (percent < 0 || percent > 100) {
-        printf("Intensity must be between 0 and 100\n");
+        fprintf(stderr, "Intensity must be between 0 and 100\n");
         return -1;
     }
     
@@ -65,7 +67,7 @@ int set_intensity(int percent) {
 
 int set_frequency(int frequency) {
     if (frequency < 0 || frequency > 1000000) {
-        printf("Frequency must be between 0 and 1000000\n");
+        fprintf(stderr, "Frequency must be between 0 and 1000000\n");
         return -1;
     }
     state.frequency = frequency;
@@ -77,7 +79,7 @@ int set_frequency(int frequency) {
 
 int set_direction(int direction) {
     if (direction > 1 || direction < 0) {
-        printf("invalid direction");
+        fprintf(stderr, "invalid direction\n");
         return -1;
     }
     
@@ -90,7 +92,7 @@ int set_direction(int direction) {
 
 int set_status(int status) {
     if (status > 1 || status < 0) {
-        printf("invalid status");
+        fprintf(stderr, "invalid status\n");
         return -1;
     }
     state.running = status;
@@ -104,5 +106,8 @@ int set_status(int status) {
 
 int gvs_emergency_stop() {
     gvs_stop();
+    if (pi_handle >= 0) {
+        pigpio_stop(pi_handle);
+    }
     return 0;
 }
